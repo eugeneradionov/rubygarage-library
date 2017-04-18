5a207bdfce4220f01aeacd99f07f2c385a1b99df
Yevhen Radionov <eugene.radionov@gmail.com>
2017-04-18 14:21:32 +0300

    Refactoring.

--- classes/Order.rb
+++ classes/Order.rb
@@ -9,6 +9,7 @@ class Order
   end
 
   def to_s
-    "Book: #{book}, Reader: #{reader.name}, Date: #{@date.strftime('%Y %b %d, %H:%M')}"
+    "Book: #{book}, Reader: #{reader.name}, "\
+    "Date: #{@date.strftime('%Y %b %d, %H:%M')}"
   end
 end
