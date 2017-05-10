b340a43251b0778462bf238723a0763c0a6a1670
Yevhen Radionov <eugene.radionov@gmail.com>
2017-05-10 22:09:42 +0300

    Renamed to book.rb.

---
+++ classes/book.rb
@@ -0,0 +1,13 @@
+# Books that are stored in the Library
+class Book
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+
+  def to_s
+    "#{title} by #{author}"
+  end
+end
