25a1fd4564e1c8fcde704ac9b1cab03e2d78eb14
Yevhen Radionov <eugene.radionov@gmail.com>
2017-05-10 22:09:27 +0300

    Renamed to reader.rb.

---
+++ classes/reader.rb
@@ -0,0 +1,16 @@
+# Reader who takes the books
+class Reader
+  attr_accessor :name, :email, :city, :street, :house
+
+  def initialize(name, email, city, street, house)
+    @name = name
+    @email = email
+    @city = city
+    @street = street
+    @house = house
+  end
+
+  def to_s
+    "#{name} - #{email}. Address: #{city}, #{house} #{street}"
+  end
+end
